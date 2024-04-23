f29f0f7ba915a480637e04f805f42e96586d7b49
dahlend <dardahlen@gmail.com>
2024-04-23 16:08:47 -0700

    improve mission phase checks

--- src/tests/test_wise.py
+++ src/tests/test_wise.py
@@ -1,32 +1,34 @@
 from neospy.wise import (
     MISSION_PHASES,
-    WISE_phase_from_jd,
-    WISE_phase_from_scan,
+    mission_phase_from_jd,
+    mission_phase_from_scan,
 )
 
 
-def test_WISE_phase_from_jd():
+def test_mission_phase_from_jd():
     for phase in MISSION_PHASES.values():
-        p0 = WISE_phase_from_jd(phase.jd_start)
+        p0 = mission_phase_from_jd(phase.jd_start)
         assert p0 == phase, p0.name + "  " + phase.name
-        p1 = WISE_phase_from_jd(phase.jd_end - 1)
+        p1 = mission_phase_from_jd(phase.jd_end - 1)
         assert p1 == phase, p1.name + "  " + phase.name
 
-    assert WISE_phase_from_jd(1000) is None
+    assert mission_phase_from_jd(1000) is None
 
 
-def test_WISE_phase_from_scan():
-    for letter in "rst":
+def test_mission_phase_from_scan():
+    for letter in "rs":
         scan_id = "10000" + letter
-        assert WISE_phase_from_scan(scan_id) == MISSION_PHASES["Reactivation_2022"]
-        scan_id = "45687" + letter
-        assert WISE_phase_from_scan(scan_id) is None
+        assert mission_phase_from_scan(scan_id) == MISSION_PHASES["Reactivation_2019"]
+        scan_id = "57042" + letter
+        assert mission_phase_from_scan(scan_id) is None
 
     scan_id = "01000a"
-    assert WISE_phase_from_scan(scan_id) == MISSION_PHASES["Cryo"]
-    scan_id = "08000k"
-    assert WISE_phase_from_scan(scan_id) == MISSION_PHASES["3-Band"]
+    assert mission_phase_from_scan(scan_id) == MISSION_PHASES["Cryo"]
+    scan_id = "08000b"
+    assert mission_phase_from_scan(scan_id) == MISSION_PHASES["3-Band"]
     scan_id = "09000a"
-    assert WISE_phase_from_scan(scan_id) == MISSION_PHASES["Post-Cryo"]
+    assert mission_phase_from_scan(scan_id) == MISSION_PHASES["Post-Cryo"]
     scan_id = "49000a"
-    assert WISE_phase_from_scan(scan_id) == MISSION_PHASES["Reactivation_2014"]
+    assert mission_phase_from_scan(scan_id) == MISSION_PHASES["Reactivation_2014"]
+    scan_id = "46000s"
+    assert mission_phase_from_scan(scan_id) == MISSION_PHASES["Reactivation_2022"]
