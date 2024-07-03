b8f6f9c8f5da43a502437db46dce9c922bde9f77
dahlend <dardahlen@gmail.com>
2024-07-03 13:49:06 -0700

    Simplify Time representation

--- src/neospy_core/src/time/mod.rs
+++ src/neospy_core/src/time/mod.rs
@@ -3,56 +3,15 @@
 //! See [TimeScale] for a list of supported Time Scales.
 //! See [Time] for the representation of time itself.
 
-use leap_second::tai_to_utc_offset;
+use std::marker::PhantomData;
+
 pub mod leap_second;
+pub mod scales;
 
-/// Available Time Scales
-///
-/// TT Varies from TDB by up to about 1.7 ms per year in a period manner.
-/// This correction is a complicated relationship due to relativistic motion of
-/// the observer on Earth vs the rest of the solar system. For performance reasons,
-/// the conversion from TDB to TT is unnecessary, as they are never more than about
-/// 2 ms apart over a century.
-///
-#[derive(Debug, PartialEq, Default, Copy, Clone)]
-pub enum TimeScale {
-    /// TDB Scaled JD time.
-    ///
-    /// This is in good agreement with "Ephemeris Time" - Which is commonly referred
-    /// to as the time units used by the JPL Ephemeris, and is used in NEOSpy as the
-    /// base time scaling.
-    ///
-    /// This is essentially the rate of time from an observer not on the surface of
-    /// the Earth (and as a result doesn't feel the relativistic dilation effects of
-    /// Earth).
-    ///
-    /// This is in agreement with TT up to about 1.7ms per century.
-    #[default]
-    TDB,
+use chrono::{DateTime, Datelike, Timelike};
+use scales::{TimeScale, JD_TO_MJD, TAI, TDB, UTC};
 
-    /// TAI Time
-    /// This is the international standard for the measurement of time.
-    /// Atomic clocks around the world keep track of this time, which then gets
-    /// converted to UTC time which is commonly used. UTC Time is an approximation
-    /// of the current time with corrections for the rotation rate of the Earth.
-    ///
-    /// As the Earth actually varies in its rotation speed,
-    TAI,
-
-    /// UTC Scaled JD time.
-    UTC,
-}
-
-/// Offset from TT to TAI.
-/// Definitional offset from TT to TAI, however we treat TT==TDB.
-///
-/// TT = TAI + TT_TO_TAI
-/// TAI = TT - TT_TO_TAI
-const TT_TO_TAI: f64 = 32.184 / 86400.0;
-
-/// Offset from JD to MJD
-/// MJD = JD + JD_TO_MJD;
-const JD_TO_MJD: f64 = -2400000.5;
+use crate::prelude::NEOSpyError;
 
 /// Representation of Time.
 ///
@@ -72,83 +31,36 @@ const JD_TO_MJD: f64 = -2400000.5;
 /// Any conversions to a single float will by necessity result in some small accuracy
 /// loss due to the nature of the representation of numbers on computers.
 ///
-#[derive(Debug, Copy, Clone)]
-pub struct Time {
-    /// Float Date
-    julian_date: f64,
+#[derive(Debug, Clone, Copy)]
 
-    /// Time scaling.
-    scale: TimeScale,
+pub struct Time<T: TimeScale> {
+    /// Julian Date
+    pub jd: f64,
+
+    scale_type: PhantomData<T>,
 }
 
-impl Time {
-    /// Construct a new Time object.
-    pub fn new(jd: f64, scale: TimeScale) -> Self {
-        Self {
-            julian_date: jd,
-            scale,
-        }
-    }
+/// Convert Hours/Minutes/Seconds/nanosecond to fraction of a day
+pub fn hour_min_sec_to_day(h: u32, m: u32, s: u32, ns: u32) -> f64 {
+    h as f64 / 24.0
+        + m as f64 / 60. / 24.
+        + s as f64 / 60. / 60. / 24.
+        + ns as f64 / 1000. / 60. / 60. / 24.
+}
 
-    /// Create Time from an Modified Julian Date (MJD).
-    pub fn from_mjd(mjd: f64, scale: TimeScale) -> Self {
-        Self {
-            julian_date: mjd - JD_TO_MJD,
-            scale,
-        }
-    }
+impl Time<UTC> {
+    /// Read time from the standard ISO  format for time.
+    pub fn from_utc_iso(s: &str) -> Result<Self, NEOSpyError> {
+        let time = DateTime::parse_from_rfc3339(s)?.to_utc();
 
-    /// Time as Julian Date float.
-    pub fn jd(&self) -> &f64 {
-        &self.julian_date
-    }
-
-    /// Convert to an MJD float.
-    pub fn mjd(&self) -> f64 {
-        self.julian_date + JD_TO_MJD
-    }
-
-    /// Rescale time to the desired time scaling.
-    pub fn mut_rescale(&mut self, scale: TimeScale) {
-        if self.scale == scale {
-            return;
-        }
-
-        // Scale time to be in TDB
-        match &self.scale {
-            TimeScale::TDB => (),
-            TimeScale::TAI => {
-                self.julian_date += TT_TO_TAI;
-            }
-            TimeScale::UTC => {
-                // move to tai first
-                // Guess the tai time that this UTC time came from
-                let offset = tai_to_utc_offset(&(self.julian_date + JD_TO_MJD));
-
-                // use that guess to update the TAI guess to fix the leap second offset
-                let offset = tai_to_utc_offset(&(self.julian_date + JD_TO_MJD + offset));
-                self.julian_date += offset;
-
-                // then tai to tdb
-                self.julian_date += TT_TO_TAI;
-            }
-        };
-
-        // Scale time from TDB to target scale
-        match scale {
-            TimeScale::TDB => (),
-            TimeScale::TAI => self.julian_date -= TT_TO_TAI,
-            TimeScale::UTC => {
-                // convert from TDB to TAI
-                self.julian_date -= TT_TO_TAI;
-
-                // Time is now TAI
-                // calculate leap seconds for that time to convert from TAI to UTC
-                let offset = tai_to_utc_offset(&(self.julian_date + JD_TO_MJD));
-                self.julian_date -= offset;
-            }
-        };
-        self.scale = scale;
+        let frac_day =
+            hour_min_sec_to_day(time.hour(), time.minute(), time.second(), time.nanosecond());
+        Ok(Time::<UTC>::from_year_month_day(
+            time.year() as i64,
+            time.month(),
+            time.day(),
+            frac_day,
+        ))
     }
 
     /// Return the Gregorian year, month, day, and fraction of a day.
@@ -157,8 +69,8 @@ impl Time {
     /// "A Machine Algorithm for Processing Calendar Dates"
     /// https://doi.org/10.1145/364096.364097
     ///
-    pub fn year_month_day(&self) -> (i64, u8, u8, f64) {
-        let offset = self.julian_date + 0.5;
+    pub fn year_month_day(&self) -> (i64, u32, u32, f64) {
+        let offset = self.jd + 0.5;
         let frac_day = offset.rem_euclid(1.0);
 
         let mut l = offset.div_euclid(1.0) as i64 + 68569;
@@ -173,7 +85,7 @@ impl Time {
 
         let month = k + 2 - 12 * l;
         let year = 100 * (n - 49) + i + l;
-        (year, month as u8, day as u8, frac_day)
+        (year, month as u32, day as u32, frac_day)
     }
 
     /// Create Time from the date in the Gregorian calendar.
@@ -182,13 +94,7 @@ impl Time {
     /// "A Machine Algorithm for Processing Calendar Dates"
     /// https://doi.org/10.1145/364096.364097
     ///
-    pub fn from_year_month_day(
-        year: i64,
-        month: u8,
-        day: u8,
-        frac_day: f64,
-        scale: TimeScale,
-    ) -> Self {
+    pub fn from_year_month_day(year: i64, month: u32, day: u32, frac_day: f64) -> Self {
         let day = day as i64 + frac_day.div_euclid(1.0) as i64;
         let frac_day = frac_day.rem_euclid(1.0);
         let month = month as i64;
@@ -197,45 +103,77 @@ impl Time {
         let days = day - 32075 + 1461 * (year + 4800 + tmp) / 4 + 367 * (month - 2 - tmp * 12) / 12
             - 3 * ((year + 4900 + tmp) / 100) / 4;
 
-        Self {
-            scale,
-            julian_date: days as f64 + frac_day,
-        }
+        Self::new(days as f64 + frac_day)
     }
 }
 
-impl From<f64> for Time {
+impl<T: TimeScale> Time<T> {
+    /// Construct a new Time object.
+    pub fn new(jd: f64) -> Self {
+        Self {
+            jd,
+            scale_type: PhantomData,
+        }
+    }
+
+    /// Create Time from an Modified Julian Date (MJD).
+    pub fn from_mjd(mjd: f64) -> Self {
+        Self {
+            jd: mjd - JD_TO_MJD,
+            scale_type: PhantomData,
+        }
+    }
+
+    /// Cast to UTC scaled time.
+    pub fn utc(&self) -> Time<UTC> {
+        Time::<UTC>::new(UTC::from_tdb(T::to_tdb(self.jd)))
+    }
+
+    /// Cast to TAI scaled time.
+    pub fn tai(&self) -> Time<TAI> {
+        Time::<TAI>::new(TAI::from_tdb(T::to_tdb(self.jd)))
+    }
+
+    /// Cast to TDB scaled time.
+    pub fn tdb(&self) -> Time<TDB> {
+        Time::<TDB>::new(T::to_tdb(self.jd))
+    }
+
+    /// Convert to an MJD float.
+    pub fn mjd(&self) -> f64 {
+        self.jd + JD_TO_MJD
+    }
+}
+
+impl<T: TimeScale> From<f64> for Time<T> {
     fn from(value: f64) -> Self {
-        Time::new(value, Default::default())
+        Time::<T>::new(value)
     }
 }
 
 #[cfg(test)]
 mod tests {
+
+    use scales::TT_TO_TAI;
+
     use super::*;
 
     #[test]
     fn test_time() {
-        let t = Time {
-            scale: TimeScale::TAI,
-            julian_date: 2451545.,
-        };
+        let t = Time::<UTC>::new(2451545.);
         assert!(t.year_month_day() == (2000, 1, 1, 0.5));
 
-        let t2 = Time::from_year_month_day(2000, 1, 1, 0.5, TimeScale::TAI);
-        assert!(t2.julian_date == 2451545.5);
+        let t2 = Time::<UTC>::from_year_month_day(2000, 1, 1, 0.5);
+        assert!(t2.jd == 2451545.5);
 
-        let t2 = Time::from_year_month_day(2000, 1, 2, -0.5, TimeScale::TAI);
-        assert!(t2.julian_date == 2451545.5);
+        let t2 = Time::<UTC>::from_year_month_day(2000, 1, 2, -0.5);
+        assert!(t2.jd == 2451545.5);
 
-        let t = Time {
-            scale: TimeScale::TAI,
-            julian_date: 2000000.,
-        };
+        let t = Time::<UTC>::new(2000000.);
         assert!(t.year_month_day() == (763, 9, 18, 0.5));
 
-        let t2 = Time::from_year_month_day(763, 9, 18, 0., TimeScale::TAI);
-        assert!(t2.julian_date == 2000000.);
+        let t2 = Time::<UTC>::from_year_month_day(763, 9, 18, 0.);
+        assert!(t2.jd == 2000000.);
     }
 
     #[test]
@@ -243,9 +181,9 @@ mod tests {
         for offset in -1000..1000 {
             let offset = offset as f64 / 10.0;
             let mjd = 41683.0 + offset / 86400.0; // TIME IN TAI
-            let mut t = Time::from_mjd(mjd, TimeScale::TAI);
-            t.mut_rescale(TimeScale::TDB);
-            t.mut_rescale(TimeScale::TAI);
+            let t = Time::<TAI>::from_mjd(mjd);
+            let t = t.tdb();
+            let t = t.tai();
 
             // Numerical precision of times near J2000 is only around 1e-10
             assert!((t.mjd() - mjd).abs() < 1e-9,);
@@ -255,9 +193,9 @@ mod tests {
         for offset in -1000..1000 {
             let offset = offset as f64 / 10.0;
             let mjd = 41683.0 + offset / 86400.0; // TIME IN TAI
-            let mut t = Time::from_mjd(mjd, TimeScale::UTC);
-            t.mut_rescale(TimeScale::TAI);
-            t.mut_rescale(TimeScale::UTC);
+            let t = Time::<UTC>::from_mjd(mjd);
+            let t = t.tai();
+            let t = t.utc();
 
             // Numerical precision of times near J2000 is only around 1e-10
             assert!(
@@ -273,9 +211,9 @@ mod tests {
             let offset = offset as f64 / 10.0;
 
             let mjd = 41683.0 + offset / 86400.0 + TT_TO_TAI;
-            let mut t = Time::from_mjd(mjd, TimeScale::UTC);
-            t.mut_rescale(TimeScale::TAI);
-            t.mut_rescale(TimeScale::UTC);
+            let t = Time::<UTC>::from_mjd(mjd);
+            let t = t.tai();
+            let t = t.utc();
 
             // Numerical precision of times near J2000 is only around 1e-10
             assert!(
