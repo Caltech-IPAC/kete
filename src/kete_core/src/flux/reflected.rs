b4ecc86ff79f2250aece2c8459300d23b43514a9
dahlend <dardahlen@gmail.com>
2024-10-18 10:51:52 -0700

    add citation

--- src/kete_core/src/flux/reflected.rs
+++ src/kete_core/src/flux/reflected.rs
@@ -7,7 +7,14 @@ use crate::{
 use nalgebra::Vector3;
 use serde::{Deserialize, Serialize};
 
-///  This computes the phase curve correction using the IAU standard for the HG model.
+/// This computes the phase curve correction using the IAU standard for the HG model.
+/// 
+/// Specifically page Page 550 - Equation (A4):
+/// 
+///     Asteroids II. University of Arizona Press, Tucson, pp. 524–556.
+///     Bowell, E., Hapke, B., Domingue, D., Lumme, K., Peltoniemi, J., Harris,
+///     A.W., 1989. Application of photometric models to asteroids, in: Binzel,
+///     R.P., Gehrels, T., Matthews, M.S. (Eds.),
 ///
 /// # Arguments
 ///
@@ -30,6 +37,13 @@ pub fn hg_phase_curve_correction(g_param: f64, phase: f64) -> f64 {
 ///
 /// H, Albedo, and Diameter are all related by the relation:
 /// diameter = c_hg / albedo.sqrt() * (10f64).powf(-h_mag / 5.0);
+/// 
+/// Specifically page Page 549 - Equation (A1) of:
+/// 
+///     Asteroids II. University of Arizona Press, Tucson, pp. 524–556.
+///     Bowell, E., Hapke, B., Domingue, D., Lumme, K., Peltoniemi, J., Harris,
+///     A.W., 1989. Application of photometric models to asteroids, in: Binzel,
+///     R.P., Gehrels, T., Matthews, M.S. (Eds.),
 ///
 /// # Arguments
 /// * `desig` - Designation of the object.
