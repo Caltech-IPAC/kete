d8170211000b3040d2a9ba09c18175aeef0559b8
Dar Dahlen <dahlend@users.noreply.github.com>
2024-10-25 08:44:52 -0700

    Add RA/DEC + rates to `SimultaneousStates` (#144)
    Add RA/DEC + rates to SimultaneousStates

--- src/kete_core/src/simult_states.rs
+++ src/kete_core/src/simult_states.rs
@@ -2,8 +2,11 @@
 //! These primarily exist to allow for easy read/write to binary formats.
 
 use crate::fov::FOV;
+use crate::frames::to_lat_lon;
 use crate::io::FileIO;
 use crate::prelude::{Error, Frame, KeteResult, State};
+use nalgebra::Vector3;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use serde::{Deserialize, Serialize};
 
 /// Collection of [`State`] at the same time.
@@ -72,5 +75,47 @@ impl SimultaneousStates {
             frame,
             fov,
         })
+    }
+
+    /// Compute RA/Dec along with linearized rates.
+    ///
+    /// Returns a vector containing [ra, dec, ra' * cos(dec), dec'], all vectors
+    /// are automatically cast into the equatorial frame.
+    /// The returned RA rate is scaled by cos(dec) so that it is equivalent to a
+    /// linear projection onto the observing plane.
+    pub fn ra_dec_with_rates(&self) -> KeteResult<Vec<[f64; 4]>> {
+        if self.fov.is_none() {
+            return Err(Error::ValueError(
+                "Field of view must be specified for the ra/dec to be computed.".into(),
+            ));
+        }
+        let fov = self.fov.as_ref().unwrap();
+
+        let mut obs = fov.observer().clone();
+        obs.try_change_frame_mut(Frame::Equatorial)?;
+
+        let obs_pos = Vector3::from(obs.pos);
+        let obs_vel = Vector3::from(obs.vel);
+
+        Ok(self
+            .states
+            .par_iter()
+            .map(|state| {
+                let mut state = state.clone();
+                state.try_change_frame_mut(Frame::Equatorial).unwrap();
+                let pos_diff = Vector3::from(state.pos) - obs_pos;
+                let vel_diff = Vector3::from(state.vel) - obs_vel;
+
+                let d_ra = (pos_diff.x * vel_diff.y - pos_diff.y * vel_diff.x)
+                    / (pos_diff.x.powi(2) + pos_diff.y.powi(2));
+                let r2 = pos_diff.norm_squared();
+
+                let d_dec = (vel_diff.z - pos_diff.z * pos_diff.dot(&vel_diff) / r2)
+                    / (r2 - pos_diff.z.powi(2)).sqrt();
+                let (dec, ra) = to_lat_lon(pos_diff[0], pos_diff[1], pos_diff[2]);
+
+                [ra, dec, d_ra * dec.cos(), d_dec]
+            })
+            .collect())
     }
 }
