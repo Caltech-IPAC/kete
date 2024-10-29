e4d8502d2e2f87b26f072c2645459d358ddc863d
Dar Dahlen <dardahlen@gmail.com>
2024-10-29 10:24:33 -0700

    rewrite parquet read to not have memory copies

--- src/kete_core/src/io/parquet.rs
+++ src/kete_core/src/io/parquet.rs
@@ -1,11 +1,11 @@
 //! Optional feature support for reading/writing parquet table
 //!
 
-use itertools::izip;
 use itertools::Itertools;
 use std::fs::File;
 
 use crate::errors::{Error, KeteResult};
+use crate::frames::Frame;
 use crate::state::State;
 
 use polars::prelude::*;
@@ -67,85 +67,80 @@ pub fn write_states_parquet(states: &[State], filename: &str) -> KeteResult<()> 
     Ok(())
 }
 
-/// Write a collection of states to a parquet table.
+/// Read a collection of states from a parquet table.
 pub fn read_states_parquet(filename: &str) -> KeteResult<Vec<State>> {
+    // this reads the parquet table, then creates iterators over the contents, making
+    // states by going through the iterators one at a time.
     let r = File::open(filename)?;
     let reader = ParquetReader::new(r);
-    let dataframe = reader.finish().map_err(|_| {
+    let mut dataframe = reader.finish().map_err(|_| {
         Error::IOError("Failed to read contents of file as a parquet table.".into())
     })?;
+    let dataframe = dataframe.as_single_chunk_par();
 
-    // dataframe.as_single_chunk_par();
+    // create all the iterators, these are all type dependant, so they get special cased
+    let mut desig_iter = dataframe
+        .column("desig")
+        .map_err(|_| Error::IOError("File doesn't contain the correct columns".into()))?
+        .str()
+        .expect("Designations are not all strings.")
+        .into_no_null_iter();
 
-    let cols = [
-        "desig", "jd", "x", "y", "z", "vx", "vy", "vz", "frame", "center",
-    ];
+    let mut frame_iter = dataframe
+        .column("frame")
+        .map_err(|_| Error::IOError("File doesn't contain the correct columns".into()))?
+        .i32()
+        .expect("Frames are not all ints.")
+        .into_no_null_iter();
 
-    if let Ok(columns) = dataframe.columns(cols) {
-        let desigs: Vec<&str> = columns[0]
-            .str()
-            .expect("Designations are not all strings.")
-            .into_no_null_iter()
-            .collect();
-        let jd: Vec<f64> = columns[1]
-            .f64()
-            .expect("JD are not all floats.")
-            .into_no_null_iter()
-            .collect();
-        let x: Vec<f64> = columns[2]
-            .f64()
-            .expect("positions are not all floats.")
-            .into_no_null_iter()
-            .collect();
-        let y: Vec<f64> = columns[3]
-            .f64()
-            .expect("positions are not all floats.")
-            .into_no_null_iter()
-            .collect();
-        let z: Vec<f64> = columns[4]
-            .f64()
-            .expect("positions are not all floats.")
-            .into_no_null_iter()
-            .collect();
-        let vx: Vec<f64> = columns[5]
-            .f64()
-            .expect("velocities are not all floats.")
-            .into_no_null_iter()
-            .collect();
-        let vy: Vec<f64> = columns[6]
-            .f64()
-            .expect("velocities are not all floats.")
-            .into_no_null_iter()
-            .collect();
-        let vz: Vec<f64> = columns[7]
-            .f64()
-            .expect("velocities are not all floats.")
-            .into_no_null_iter()
-            .collect();
-        let frame: Vec<i32> = columns[8]
-            .i32()
-            .expect("Frames are not formatted correctly.")
-            .into_no_null_iter()
-            .collect();
-        let centers: Vec<i64> = columns[9]
-            .i64()
-            .expect("Centers are not formatted correctly.")
-            .into_no_null_iter()
-            .collect();
-        return Ok(izip!(desigs, jd, x, y, z, vx, vy, vz, frame, centers)
-            .map(|(d, j, x0, y0, z0, vx0, vy0, vz0, f, c)| {
-                State::new(
-                    crate::state::Desig::Name(d.to_string()),
-                    j,
-                    [x0, y0, z0].into(),
-                    [vx0, vy0, vz0].into(),
-                    f.into(),
-                    c,
-                )
-            })
-            .collect());
-    };
-    Err(Error::IOError(
-        "Parquet file is not a states file, missing columns.".into(),
-    ))
+    let mut center_iter = dataframe
+        .column("center")
+        .map_err(|_| Error::IOError("File doesn't contain the correct columns".into()))?
+        .i64()
+        .expect("Centers are not all ints.")
+        .into_no_null_iter();
+
+    // the remaining columns are all floats, so here we make a vector of iterators of
+    // floats
+    let mut state_iters = dataframe
+        .columns(["jd", "x", "y", "z", "vx", "vy", "vz"])
+        .map_err(|_| Error::IOError("File doesn't contain the correct columns".into()))?
+        .iter()
+        .map(|s| {
+            s.f64()
+                .expect("state information is not all floats.")
+                .into_no_null_iter()
+        })
+        .collect::<Vec<_>>();
+
+    // throw in an assert, hopefully the compiler can see that the dimension of this is
+    // then 7, meaning the index lookups below are not required to be bounds checked.
+    assert!(state_iters.len() == 7);
+
+    Ok((0..dataframe.height())
+        .map(|_| {
+            let desig = desig_iter
+                .next()
+                .expect("should have as many iterations as rows");
+            let frame: Frame = frame_iter.next().unwrap().into();
+            let center_id = center_iter.next().unwrap();
+
+            let jd = state_iters[0].next().unwrap();
+            let x = state_iters[1].next().unwrap();
+            let y = state_iters[2].next().unwrap();
+            let z = state_iters[3].next().unwrap();
+            let vx = state_iters[4].next().unwrap();
+            let vy = state_iters[5].next().unwrap();
+            let vz = state_iters[6].next().unwrap();
+
+            State::new(
+                crate::state::Desig::Name(desig.to_string()),
+                jd,
+                [x, y, z].into(),
+                [vx, vy, vz].into(),
+                frame,
+                center_id,
+            )
+        })
+        .collect())
 }
