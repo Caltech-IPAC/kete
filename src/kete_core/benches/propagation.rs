d532376364051668472b0ad318a863d09d7a99f6
dahlend <dardahlen@gmail.com>
2024-11-16 11:04:29 -0800

    bench

--- src/kete_core/benches/propagation.rs
+++ src/kete_core/benches/propagation.rs
@@ -9,99 +9,71 @@ use pprof::criterion::{Output, PProfProfiler};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 lazy_static! {
-    static ref CIRCULAR: State = {
+    static ref CIRCULAR: State<Ecliptic> = {
         State::new(
             Desig::Name("Circular".into()),
             2451545.0,
             [0.0, 1., 0.0].into(),
             [-constants::GMS_SQRT, 0.0, 0.0].into(),
-            Frame::Ecliptic,
             0,
         )
     };
-    static ref ELLIPTICAL: State = {
+    static ref ELLIPTICAL: State<Ecliptic> = {
         State::new(
             Desig::Name("Elliptical".into()),
             2451545.0,
             [0.0, 1.5, 0.0].into(),
             [-constants::GMS_SQRT, 0.0, 0.0].into(),
-            Frame::Ecliptic,
             0,
         )
     };
-    static ref PARABOLIC: State = {
+    static ref PARABOLIC: State<Ecliptic> = {
         State::new(
             Desig::Name("Parabolic".into()),
             2451545.0,
             [0.0, 2., 0.0].into(),
             [-constants::GMS_SQRT, 0.0, 0.0].into(),
-            Frame::Ecliptic,
             0,
         )
     };
-    static ref HYPERBOLIC: State = {
+    static ref HYPERBOLIC: State<Ecliptic> = {
         State::new(
             Desig::Name("Hyperbolic".into()),
             2451545.0,
             [0.0, 3., 0.0].into(),
             [-constants::GMS_SQRT, 0.0, 0.0].into(),
-            Frame::Ecliptic,
             0,
         )
     };
 }
 
-fn prop_n_body_radau(state: State, dt: f64) {
+fn prop_n_body_radau(state: State<Ecliptic>, dt: f64) {
     let jd = state.jd + dt;
-    propagation::propagate_n_body_spk(state, jd, false, None).unwrap();
+    propagation::propagate_n_body_spk(state.into_frame(), jd, false, None).unwrap();
 }
 
-fn prop_n_body_vec_radau(mut state: State, dt: f64) {
+fn prop_n_body_vec_radau(mut state: State<Ecliptic>, dt: f64) {
     let spk = get_spk_singleton().read().unwrap();
     spk.try_change_center(&mut state, 10).unwrap();
-    state.try_change_frame_mut(Frame::Ecliptic).unwrap();
     let states = vec![state.clone(); 100];
     let non_gravs = vec![None; 100];
     let jd = state.jd + dt;
     propagation::propagate_n_body_vec(states, jd, None, non_gravs).unwrap();
 }
 
-fn prop_n_body_radau_par(state: State, dt: f64) {
-    let states: Vec<State> = (0..100).map(|_| state.clone()).collect();
-    let _tmp: Vec<State> = states
+fn prop_n_body_radau_par(state: State<Ecliptic>, dt: f64) {
+    let states: Vec<State<_>> = (0..100).map(|_| state.clone()).collect();
+    let _tmp: Vec<State<_>> = states
         .into_par_iter()
         .map(|s| {
             let jd = s.jd + dt;
-            propagation::propagate_n_body_spk(s, jd, false, None).unwrap()
+            propagation::propagate_n_body_spk(s.into_frame(), jd, false, None).unwrap()
         })
         .collect();
 }
 
-fn prop_2_body_radau(state: State, dt: f64) {
-    propagation::propagation_central(&state, state.jd + dt).unwrap();
-}
-
-fn prop_2_body_kepler(state: State, dt: f64) {
+fn prop_2_body_kepler(state: State<Equatorial>, dt: f64) {
     propagation::propagate_two_body(&state, state.jd + dt).unwrap();
-}
-
-pub fn two_body_numeric(c: &mut Criterion) {
-    let mut twobody_num_group = c.benchmark_group("2-Body-Numeric");
-
-    for state in [
-        CIRCULAR.clone(),
-        ELLIPTICAL.clone(),
-        PARABOLIC.clone(),
-        HYPERBOLIC.clone(),
-    ] {
-        let name = match &state.desig {
-            Desig::Name(n) => n,
-            _ => panic!(),
-        };
-        twobody_num_group.bench_with_input(BenchmarkId::new("Single", name), &state, |b, s| {
-            b.iter(|| prop_2_body_radau(black_box(s.clone()), black_box(1000.0)))
-        });
-    }
 }
 
 pub fn n_body_prop(c: &mut Criterion) {
@@ -159,12 +131,12 @@ pub fn two_body_analytic(c: &mut Criterion) {
             _ => panic!(),
         };
         twobody_group.bench_with_input(BenchmarkId::new("Single", name), &state, |b, s| {
-            b.iter(|| prop_2_body_kepler(s.clone(), black_box(1000.0)))
+            b.iter(|| prop_2_body_kepler(s.clone().into_frame(), black_box(1000.0)))
         });
     }
 }
 
 criterion_group!(name=benches;
                  config = Criterion::default().sample_size(30).measurement_time(Duration::from_secs(15)).with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-                 targets=n_body_prop_vec, two_body_analytic, n_body_prop, two_body_numeric);
+                 targets=n_body_prop_vec, two_body_analytic, n_body_prop);
 criterion_main!(benches);
--- src/kete_core/benches/spice.rs
+++ src/kete_core/benches/spice.rs
@@ -1,16 +1,16 @@
 extern crate criterion;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use kete_core::{spice::get_spk_singleton, state::State};
+use kete_core::{frames::{Ecliptic, Equatorial}, spice::get_spk_singleton, state::State};
 use pprof::criterion::{Output, PProfProfiler};
 
 fn spice_get_raw_state(jd: f64) {
     let spice = get_spk_singleton().try_read().unwrap();
     for _ in 0..1000 {
-        let _ = spice.try_get_raw_state(5, jd).unwrap();
+        let _ = spice.try_get_raw_state::<Equatorial>(5, jd).unwrap();
     }
 }
 
-fn spice_change_center(mut state: State) {
+fn spice_change_center(mut state: State<Ecliptic>) {
     let spice = get_spk_singleton().try_read().unwrap();
     for _ in 0..500 {
         spice.try_change_center(&mut state, 10).unwrap();
@@ -22,7 +22,7 @@ fn spice_get_state(jd: f64) {
     let spice = get_spk_singleton().try_read().unwrap();
     for _ in 0..1000 {
         let _ = spice
-            .try_get_state(5, jd, 10, kete_core::frames::Frame::Ecliptic)
+            .try_get_state::<Ecliptic>(5, jd, 10)
             .unwrap();
     }
 }
@@ -30,7 +30,7 @@ fn spice_get_state(jd: f64) {
 pub fn spice_benchmark(c: &mut Criterion) {
     let spice = get_spk_singleton().try_read().unwrap();
     let state = spice
-        .try_get_state(5, 2451545.0, 10, kete_core::frames::Frame::Ecliptic)
+        .try_get_state::<Ecliptic>(5, 2451545.0, 10)
         .unwrap();
     c.bench_function("spice_get_raw_state", |b| {
         b.iter(|| spice_get_raw_state(black_box(2451545.0)))
